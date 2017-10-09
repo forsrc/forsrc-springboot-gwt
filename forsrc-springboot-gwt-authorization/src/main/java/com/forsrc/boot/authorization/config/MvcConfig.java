56afb5045f8908733ab3da57153215341562f5b3
forsrc <forsrc@gmail.com>
2017-10-09 17:24:57 +0900

    addCorsMappings

--- forsrc-springboot-gwt-authorization/src/main/java/com/forsrc/boot/authorization/config/MvcConfig.java
+++ forsrc-springboot-gwt-authorization/src/main/java/com/forsrc/boot/authorization/config/MvcConfig.java
@@ -1,15 +1,35 @@
 package com.forsrc.boot.authorization.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("loginhtml/login");
         registry.addViewController("/index").setViewName("loginhtml/index");
         registry.addViewController("/").setViewName("loginhtml/index");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
