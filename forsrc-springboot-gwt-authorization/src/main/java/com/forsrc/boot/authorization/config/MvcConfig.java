51a9dad481f1b2d3a9fda66331f6d9b230ac1557
yes <forsrc@gmail.com>
2017-10-10 15:50:24 +0900

    Update MvcConfig.java

--- forsrc-springboot-gwt-authorization/src/main/java/com/forsrc/boot/authorization/config/MvcConfig.java
+++ forsrc-springboot-gwt-authorization/src/main/java/com/forsrc/boot/authorization/config/MvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
