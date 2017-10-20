43858098fe1c5f70d7e6d4c0b20bafc07f2f0114
yes <forsrc@gmail.com>
2017-10-20 12:44:19 +0900

    Update TestController.java

--- forsrc-springboot-gwt-authorization/src/main/java/com/forsrc/boot/authorization/web/test/TestController.java
+++ forsrc-springboot-gwt-authorization/src/main/java/com/forsrc/boot/authorization/web/test/TestController.java
@@ -1,8 +1,12 @@
 package com.forsrc.boot.authorization.web.test;
 
+import java.io.Serializable;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jboss.cache.Cache;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +17,17 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class TestController {
+
+    @Autowired
+    private Cache<Serializable, Serializable> cache;
+
     @RequestMapping(value = "/test", method = { RequestMethod.GET, RequestMethod.POST }, produces = {
             MediaType.APPLICATION_JSON_UTF8_VALUE })
-
     public ResponseEntity<Map<String, String>> get(UriComponentsBuilder ucBuilder) {
         Map<String, String> map = new HashMap<>();
+        cache.put("/root/test", "time", new Date());
         map.put("test", "hello world");
+        map.put("time", cache.get("/root/test",  "time").toString());
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
