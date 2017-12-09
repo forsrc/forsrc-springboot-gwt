98d612848a4adf417d9df3825f7922c1cf8faf19
forsrc <forsrc@gmail.com>
2017-12-09 16:34:14 +0900

    plantuml

--- forsrc-springboot-gwt-resource/src/main/resources/public/plantuml/uml.js
+++ forsrc-springboot-gwt-resource/src/main/resources/public/plantuml/uml.js
@@ -1,15 +1,16 @@
 
 (function() {
-    var url = "/plantuml/svg?uml="
     var images = document.getElementsByTagName("img");
     var length = images.length;
     var i = 0;
     var uml = "";
     var img = null;
+    var url = null;
     for(; i < length; i++) {
         img = images[i];
         uml = img.getAttribute("uml");
         if (uml) {
+            url = img.getAttribute("src");
             img.setAttribute("src", url + encodeURI(uml));
         }
     }
