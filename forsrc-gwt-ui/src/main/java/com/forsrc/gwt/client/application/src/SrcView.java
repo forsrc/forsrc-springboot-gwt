a6e1c73b6f7cb5d41f0dee214c2ed79afaad4729
yes <forsrc@gmail.com>
2017-12-13 16:29:04 +0900

    Update SrcView.java

--- forsrc-gwt-ui/src/main/java/com/forsrc/gwt/client/application/src/SrcView.java
+++ forsrc-gwt-ui/src/main/java/com/forsrc/gwt/client/application/src/SrcView.java
@@ -15,10 +15,9 @@ import gwt.material.design.addins.client.tree.MaterialTreeItem;
 import gwt.material.design.client.constants.IconType;
 import gwt.material.design.client.ui.MaterialLabel;
 
-public class SrcView extends ViewWithUiHandlers<SrcUiHandlers>  implements SrcPresenter.MyView {
+public class SrcView extends ViewWithUiHandlers<SrcUiHandlers> implements SrcPresenter.MyView {
     interface Binder extends UiBinder<Widget, SrcView> {
     }
-
 
     @Inject
     SrcView(Binder uiBinder) {
@@ -36,11 +35,26 @@ public class SrcView extends ViewWithUiHandlers<SrcUiHandlers>  implements SrcPr
     @Override
     protected void onAttach() {
         super.onAttach();
-        MaterialTreeItem item = new MaterialTreeItem("test", IconType.FOLDER);
-        item.add(new MaterialTreeItem("A"));
-        srcTree.add(item);
         String filename = Window.Location.getHash().replaceAll(".*filename=", "");
-        //Window.alert(filename);
+        // Window.alert(filename);
         getUiHandlers().list(filename);
+    }
+
+    @Override
+    public void setSrcTree(SrcFileVo srcFileVo) {
+        MaterialTreeItem item = new MaterialTreeItem(srcFileVo.getName(),
+                srcFileVo.isFile() ? IconType.INSERT_DRIVE_FILE : IconType.FOLDER);
+        this.srcTree.add(item);
+        SrcFileVo[] list = srcFileVo.getList();
+        if (list == null) {
+            return;
+        }
+        SrcFileVo vo = null;
+        for (int i = 0; i < list.length; i++) {
+            vo = list[i];
+            MaterialTreeItem child = new MaterialTreeItem(vo.getName(),
+                    vo.isFile() ? IconType.INSERT_DRIVE_FILE : IconType.FOLDER);
+            item.add(child);
+        }
     }
 }
