ff976f715e013c362c1d00f3c2953f6706058f2a
yes <forsrc@gmail.com>
2017-12-13 16:27:47 +0900

    Create SrcFileVo.java

---
+++ forsrc-gwt-ui/src/main/java/com/forsrc/gwt/client/application/src/SrcFileVo.java
@@ -0,0 +1,69 @@
+package com.forsrc.gwt.client.application.src;
+
+import java.util.Arrays;
+
+public class SrcFileVo {
+
+    private String name;
+    private boolean exists;
+    private boolean isFile;
+    private String length;
+    private String lastModified;
+
+    private SrcFileVo[] list;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isExists() {
+        return exists;
+    }
+
+    public void setExists(boolean exists) {
+        this.exists = exists;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(String lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public boolean isFile() {
+        return isFile;
+    }
+
+    public void setFile(boolean isFile) {
+        this.isFile = isFile;
+    }
+
+    public SrcFileVo[] getList() {
+        return list;
+    }
+
+    public void setList(SrcFileVo[] list) {
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "SrcFileVo [name=" + name + ", exists=" + exists + ", isFile=" + isFile + ", length=" + length
+                + ", lastModified=" + lastModified + ", list=" + (list == null ? null : Arrays.toString(list)) + "]";
+    }
+
+}
