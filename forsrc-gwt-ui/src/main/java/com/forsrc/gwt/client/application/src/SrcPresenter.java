54012dd41794bca85342f1e31188f281e2a224db
yes <forsrc@gmail.com>
2017-12-13 16:28:36 +0900

    Update SrcPresenter.java

--- forsrc-gwt-ui/src/main/java/com/forsrc/gwt/client/application/src/SrcPresenter.java
+++ forsrc-gwt-ui/src/main/java/com/forsrc/gwt/client/application/src/SrcPresenter.java
@@ -17,7 +17,9 @@ import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
 import com.google.gwt.http.client.URL;
+import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
@@ -37,6 +39,8 @@ public class SrcPresenter extends Presenter<SrcPresenter.MyView, SrcPresenter.My
 
     interface MyView extends View, HasUiHandlers<SrcUiHandlers> {
         public void onSearch(String text);
+
+        public void setSrcTree(SrcFileVo srcFileVo);
     }
 
     @ProxyStandard
@@ -86,6 +90,37 @@ public class SrcPresenter extends Presenter<SrcPresenter.MyView, SrcPresenter.My
                     if (200 == response.getStatusCode()) {
                         JSONObject data = new JSONObject(JsonUtils.safeEval(response.getText()));
                         GWT.log(data.toString());
+                        try {
+                            SrcFileVo srcFileVo = new SrcFileVo();
+                            srcFileVo.setName(data.get("name").isString().stringValue());
+                            srcFileVo.setExists(data.get("exists").isBoolean().booleanValue());
+                            if (srcFileVo.isExists()) {
+                                srcFileVo.setExists(data.get("isFile").isBoolean().booleanValue());
+                                srcFileVo.setLength(data.get("length").isString().stringValue());
+                                srcFileVo.setLastModified(data.get("lastModified").isString().stringValue());
+                            }
+                            JSONArray jsonArray = data.get("list").isArray();
+                            if (jsonArray != null) {
+                                int size = jsonArray.size();
+                                SrcFileVo[] list = new SrcFileVo[size];
+                                JSONObject item = null;
+                                for (int i = 0; i < size; i++) {
+                                    item = jsonArray.get(i).isObject();
+                                    SrcFileVo vo = new SrcFileVo();
+                                    vo.setName(item.get("name").isString().stringValue());
+                                    vo.setExists(item.get("exists").isBoolean().booleanValue());
+                                    vo.setFile(item.get("isFile").isBoolean().booleanValue());
+                                    vo.setLength(item.get("length").isString().stringValue());
+                                    vo.setLastModified(item.get("lastModified").isString().stringValue());
+                                    list[i] = vo;
+                                }
+                                srcFileVo.setList(list);
+                            }
+                            GWT.log(srcFileVo.toString());
+                            getView().setSrcTree(srcFileVo);
+                        } catch (Exception e) {
+                            MaterialToast.fireToast("Response:" + e.getMessage());
+                        }
                     } else {
                         MaterialToast.fireToast("Response:" + response.getStatusCode());
                     }
