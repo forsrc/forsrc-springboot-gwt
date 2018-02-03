8dbff2601e12872ee1fad2c16dc3b89f8e61ffd1
forsrc <forsrc@gmail.com>
2018-02-04 04:46:59 +0900

    sso

--- forsrc-springboot-gwt-resource/src/main/java/com/forsrc/boot/resource/config/ResourceServerConfig.java
+++ forsrc-springboot-gwt-resource/src/main/java/com/forsrc/boot/resource/config/ResourceServerConfig.java
@@ -1,14 +1,24 @@
 package com.forsrc.boot.resource.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-
-//@EnableResourceServer
+@Configuration
+@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Bean
+    OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
